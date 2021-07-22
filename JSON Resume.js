d8aa4973dace9f1060986fc726d78ea9cf4bc56b
Aravindan-111 <86969801+Aravindan-111@users.noreply.github.com>
2021-07-22 16:49:06 +0530

    Add files via upload

---
+++ JSON Resume.js
@@ -0,0 +1,16 @@
+let Resume = {
+    Name: "Aravindan",
+    Age: 25,
+    Occupation: "Jobless",
+    Qualification: {
+        "10th": "StateBoard",
+        "12th": "StateBoard",
+        College: {
+            UG: "SIET",
+            PG: "Hindustan"
+        },
+    },
+    Experience: "None"
+}
+
+console.log(Resume.Qualification["10th"]);
